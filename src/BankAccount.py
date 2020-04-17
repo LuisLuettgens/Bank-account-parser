17f789b6acd2a3e0e2be3fa8596c5d6f1675de5a
Luis <luis1@uni-bremen.de>
2020-04-17 17:19:15 +0200

    copied src files into folder src

---
+++ src/BankAccount.py
@@ -0,0 +1,381 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 12:31:58 2020
+
+@author: LUL3FE
+"""
+import numpy as np
+import os
+import re
+import string
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from pathlib import Path
+import functools
+import helper as helper
+from pandas.plotting import register_matplotlib_converters
+import shelve
+
+class BankAccount:
+    def __init__(self, data_latest_file, other_data_files = []):
+        register_matplotlib_converters()
+        print('')
+        self.data_latest_file  = data_latest_file
+        self.data_other_files  = other_data_files
+        self.dfs               = []
+        print('Generating meta data...\t\t\t\t\t', end='')
+        self.meta_data_lines   = ''
+        self.meta_data         = self.get_meta_info(self.data_latest_file)
+        self.current_balance   = float(self.meta_data['Balance'].replace('.','').replace(',','.'))
+        self.currency          = self.meta_data['Currency']
+        self.bank_account_type = self.meta_data['BA_type']
+        self.IBAN              = self.meta_data['IBAN']
+        print('done!')
+        self.categories        = ['Groceries', 'Dining', 'Amazon', 'Rent', 'Mobil phone /\n Internet', 'Culture',
+                                 'Travel', 'Credit card',  'Fuel', 'Insurance', 'EoQ', 'Pharmacy', 'None']
+                
+        latest_data_file_compressed_path = self.erase_meta_data()
+        self.dfs.append(pd.read_csv(latest_data_file_compressed_path,delimiter=';', encoding ='latin-1'))
+        os.remove(latest_data_file_compressed_path)
+        
+        for data_file in self.data_other_files:
+            print('Parsing file: ' + data_file +'...\t\t\t', end='')
+            if not is_valid_csv_file(data_file):
+                raise ValueError('The input file causes problems. Please input an other file...')
+            else:
+                self.dfs.append(pd.read_csv(erase_meta_data(data_file),delimiter=';', encoding ='latin-1'))
+                print('done!')
+        
+        append_ignore_idx = functools.partial(pd.DataFrame.append,ignore_index=True)
+        
+        self.data = functools.reduce(append_ignore_idx,self.dfs)
+        self.data = valid_table(self.data,self.current_balance)
+        
+        self.daily_data = self.data[['Wertstellung','Betrag (EUR)']].groupby('Wertstellung').sum().reset_index()
+        self.daily_data = helper.add_balance_col(self.daily_data, self.current_balance)
+        
+        print('Updatig daily transactions...\t\t\t\t', end='')
+        self.daily_data = self.update_daily()
+        print('done!')
+        print('Deleting unnamed column...\t\t\t\t', end='')
+        del self.data['Unnamed: 11']
+        print('done!')
+        print('Adding labels to transactions...\t\t\t', end='')
+        self.label_row()
+        print('done!')
+        print('')
+        None_idx = self.data['Transaction Label'].value_counts().index.to_list().index('None')
+        transaction_label_vals = self.data['Transaction Label'].value_counts().values[None_idx]       
+        print('In total',
+              "{:.2f}".format((1-transaction_label_vals/self.data['Transaction Label'].shape[0])*100),
+              "% of all transactions have been labels.")
+        print('')
+        
+    def get_data(self):
+        return self.data
+    
+    def get_data_daily(self):
+        return self.daily_data()
+            
+    def get_months(self, start_date, end_date, use_daily_table=True, use_Werstellung = True):
+        if use_Werstellung:
+            if use_daily_table:
+                return self.daily_data[(self.daily_data['Wertstellung'] >= start_date) &
+                                       (self.daily_data['Wertstellung'] <= end_date)]
+            else:
+                return self.data[(self.data['Wertstellung'] >= start_date) &
+                                 (self.data['Wertstellung'] <= end_date)]
+        else:
+            if use_daily_table:
+                return self.daily_data[(self.daily_data['Buchungstag'] >= start_date) &
+                                       (self.daily_data['Buchungstag'] <= end_date)]
+            else:
+                return self.data[(self.data['Buchungstag'] >= start_date) &
+                                 (self.data['Buchungstag'] <= end_date)]
+                
+    def last_month(self):
+        return self.get_months(n_months_back(1),datetime.now(),use_daily_table=False)
+        
+    def last_month_daily(self):
+        return self.get_months(n_months_back(1),datetime.now())
+
+    
+    def last_quater(self,use_daily_table=True):
+        return self.get_months(n_months_back(3),datetime.now(),use_daily_table)
+    
+    def summary(self,start,end):
+        
+        # preparation
+        df       = self.get_months(start,end)
+        df_trans = self.get_months(start,end,use_daily_table=False)
+        
+        # Account balance & transactions
+        Wert   = df['Wertstellung']
+        Bal    = df['Balance']
+        Betrag = df['Betrag (EUR)']
+        
+        dates  = list(Wert[0:-1:int(np.floor(len(Wert)/6))])
+        xlabels =[x.date().strftime('%Y-%m-%d') for x in dates]
+        
+        # Expenses per month plot
+        expenses, total_expenses = self.cluster_expenses(*self.total_expenses(df_trans))
+        
+        # Compare expenses to previous time period
+        diff = (end-start).days
+        
+        begin_prev_period = start - timedelta(days=diff+1)
+        end_prev_period   = start - timedelta(days=1)
+
+        pref_period_trans = self.get_months(begin_prev_period,end_prev_period,use_daily_table=False)
+        diff = self.trend_adjacent(df_trans,pref_period_trans)
+        
+        # Income vs. Expenses
+        total_salary = df_trans.loc[(df_trans['Betrag (EUR)'] > 0) & (df_trans['Transaction Label'] == 'Salary')].sum()['Betrag (EUR)']  
+        other_income = df_trans.loc[(df_trans['Betrag (EUR)'] > 0) & (df_trans['Transaction Label'] != 'Salary')].sum()['Betrag (EUR)']  
+        
+        category_names = ['Salary', 'Other income', 'Expenses']
+        results = {'': [total_salary, other_income, total_expenses]}
+
+        data = np.array(list(results.values()))
+        data_cum = data.cumsum(axis=1)
+        category_colors = plt.get_cmap('Blues')(
+        np.linspace(0.15, 0.85, data.shape[1]))
+        
+        # Create plots
+        nrows = 3
+        ncols = 2
+        fig, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(ncols*10, nrows*10))
+        
+        # Accounts balance plot
+        axes[0,0].plot(Wert,Bal)
+        axes[0,0].set_xticklabels(xlabels, rotation=20)
+        axes[0,0].set_title("Account Balance")
+        
+        # Account transaction
+        axes[0,1].plot(Wert,Betrag)
+        axes[0,1].set_xticklabels(xlabels, rotation=20)
+        axes[0,1].set_title("Spendings")
+
+        # Expenses per category
+        axes[1,0].pie(expenses.values(),labels=expenses.keys(), autopct='%1.1f%%',shadow=True, startangle=90)
+        axes[1,0].axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        axes[1,0].set_title("Expenses per category")
+        
+        # Compare with last period
+        axes[1,1].bar(diff.keys(),diff.values())
+        axes[1,1].axhline(y=0.0, color='k', linestyle='-')
+        axes[1,1].set_xticklabels(diff.keys(), rotation=60)
+        axes[1,1].set_title("Compare with last period")
+    
+        # Income versus expenses
+        axes[2,0].invert_yaxis()
+        axes[2,0].xaxis.set_visible(False)
+        axes[2,0].set_xlim(0, np.sum(data, axis=1).max())
+        
+        for i, (colname, color) in enumerate(zip(category_names, category_colors)):
+            widths = data[:, i]
+            starts = data_cum[:, i] - widths
+            axes[2,0].barh('', widths, left=starts, height=0.5,
+                    label=colname, color=color)
+            xcenters = starts + widths / 2
+    
+            r, g, b, _ = color
+            text_color = 'white' if r * g * b < 0.5 else 'darkgrey'
+            for y, (x, c) in enumerate(zip(xcenters, widths)):
+                axes[2,0].text(x, y, str(int(c)) + '€', ha='center', va='center',
+                        color=text_color, fontsize='xx-large')
+                axes[2,0].legend(ncol=len(category_names), bbox_to_anchor=(0, 1), loc='lower left', fontsize='xx-large')
+    
+        # Set title
+        title = "".join(['Summay for period: ', start.date().strftime('%Y-%m-%d'), ' - ',end.date().strftime('%Y-%m-%d')])
+        fig.suptitle(title , fontsize=16)
+    
+    def summary_last_quater(self):
+        now = datetime.now()
+        last_year = now.year-1
+        prev_Q4_start = datetime(last_year,10,1,0,0,0)
+        Q1_start = datetime(now.year,1,1,0,0,0)
+        Q2_start = datetime(now.year,4,1,0,0,0)
+        Q3_start = datetime(now.year,7,1,0,0,0)
+        Q4_start = datetime(now.year,10,1,0,0,0)
+        
+        prev_Q4_end = Q1_start - timedelta(days=1)
+        Q1_end      = Q2_start - timedelta(days=1)
+        Q2_end      = Q3_start - timedelta(days=1)
+        Q3_end      = Q4_start - timedelta(days=1)
+        
+        quartals = [(prev_Q4_start,prev_Q4_end),
+                              (Q1_start,Q1_end),
+                              (Q2_start,Q2_end),
+                              (Q3_start,Q3_end)]
+        
+        return self.summary(*quartals[int((now.month-1)/3)])
+        
+        
+    def update_daily(self):
+        print('Updatig daily transactions...\t\t\t\t', end='')
+        daily_wertstellung = list(self.daily_data['Wertstellung'])
+        start_date = min(daily_wertstellung)
+        end_date   = max(daily_wertstellung)
+        days = helper.generate_days(start_date, end_date)
+        df = pd.DataFrame({'Wertstellung': [], 'Betrag (EUR)': [], 'Balance': []})
+    
+        for day in days:
+            s = pd.Series({'Wertstellung': day, 'Betrag (EUR)': 0, 'Balance': None})
+            df = df.append(s,ignore_index=True)
+               
+        for index, row in self.daily_data.iterrows():
+            WERT    = row['Wertstellung']
+            BETRAG  = row['Betrag (EUR)']
+            BALANCE = row['Balance']
+            idx = df.index[df['Wertstellung'] == WERT]
+            df.loc[idx,'Betrag (EUR)'] = BETRAG
+            df.loc[idx,'Balance']      = BALANCE
+                
+        for idx, row in df.iterrows():
+            if row['Balance'] == None:
+                df.loc[idx,'Balance'] = df.loc[idx-1,'Balance']
+        print('done!')
+        return df
+    
+    def label_rows(self):
+        print('Adding labels to transactions...\t\t\t', end='')
+        self.load_keywords_from_db()
+        for idx, row in self.data.iterrows():
+            row_df = pd.DataFrame(row).T
+            for key in (self.db).keys():
+                label = key
+                for cat_key in self.db[key].keys():
+                    col_name = cat_key
+                    if row_df[col_name].str.contains("|".join(self.db[key][cat_key]),case=False,na=False).values[0]:
+                        self.data.loc[idx,'Transaction Label'] = label
+        
+            if is_miete(row_df).values[0]:
+                self.data.loc[idx,'Transaction Label'] = 'Rent'
+        print('done!')
+        
+      
+    def total_expenses(self, df):
+        total_expenses = -df.loc[df['Betrag (EUR)'] < 0].sum()['Betrag (EUR)']
+        expenses = {}
+        for category in self.categories:
+            expenses[category] = -df.loc[(df['Betrag (EUR)'] < 0) & (df['Transaction Label'] == category)].sum()['Betrag (EUR)']
+        return expenses, total_expenses
+    
+    def cluster_expenses(self,d,total_expenses, min_quota = 0.025):
+        d['other'] = 0.
+        for key in d.copy().keys():
+            if d[key] < min_quota*total_expenses:
+                d['other'] += d[key]
+                del d[key]
+        return d, total_expenses
+    
+    def category_expenses(self, df, category):
+        return {category: -df.loc[(df['Betrag (EUR)'] < 0) & (df['Transaction Label'] == category)].sum()['Betrag (EUR)']}
+        
+    def get_category(self,category,start,end):
+        if category not in self.categories:
+            print('ERROR: This is an unknown category!\n')
+            print('Choose one of the following categories:')
+            for i, cat in enumerate(self.categories):
+                print(i,': ', cat)
+            return False
+        else:
+            df_trans = self.get_months(start,end,use_daily_table=False)
+            return df_trans[df_trans['Transaction Label'] == category]
+    
+    def trend_adjacent(self,df1, df2):
+        # assuming they are actually adjacent
+        df1_lastest = True
+        if max(df1['Wertstellung']) < max(df2['Wertstellung']):
+            df1_lastest = False
+        
+        df1_expenses, _ = self.total_expenses(df1)
+        df2_expenses, _ = self.total_expenses(df2)
+        
+        diff = {}
+        for category in self.categories:
+            diff[category] =(2*int(df1_lastest)-1)*(df1_expenses[category]- df2_expenses[category])
+        return diff
+       
+    def load_keywords_from_db(self, path='database.db'):
+        extenstions = ['.bak', '.dat', '.dir']
+        if all(list(map(lambda x: Path(path+x).is_file(),extenstions))):
+            database        = shelve.open(path)
+            self.db         = dict(database)
+            self.categories = list(self.db.keys())
+            self.categories.append('Rent')
+            self.categories.append('None')
+        else:
+            print('Could not find a file under the given path:', path)
+            raise ValueError('Could not find a file under the given path: ' + path)
+    
+    def save_data(self,path):
+        self.data.to_csv(path,sep=';',quoting=int(True), encoding ='latin-1')
+        
+        with open(path, "r") as f:
+            lines = f.readlines()
+            
+        with open(path, "w") as f:
+            for line in self.meta_data_lines:
+                f.write(line)
+            for line in lines:
+                f.write(line)
+        
+      
+    def erase_meta_data(self):
+        with open(self.data_latest_file, "r", encoding='latin_1') as f:
+            lines = f.readlines()
+    
+        header_idx = -1
+        for i, line in enumerate(lines):
+            if np.min([line.find('Buchungstag'),line.find('Wertstellung'),line.find('BLZ')])> -1:
+                header_idx = i
+        
+        if header_idx > -1:
+            self.meta_data_lines = lines[:header_idx]
+            lines = lines[header_idx:]
+        
+        with open(self.data_latest_file + 'wo_meta.csv', "w") as f:
+            for line in lines:
+                f.write(line)
+        return self.data_latest_file + 'wo_meta.csv'
+    
+    def get_meta_info(self):
+        print('Generating meta data...\t\t\t\t\t', end='')
+        self.meta_data = {}
+        
+        with open(self.data_latest_file, "r", encoding='latin_1') as f:
+            lines = f.readlines()
+        IBAN_line_pattern    = r'.+Kontonummer.+'
+        balance_line_pattern = r'.+Kontostand.+'
+    
+        found_IBAN_line    = False
+        found_balance_line = False
+    
+        for line in lines:
+            if not found_balance_line and re.findall(balance_line_pattern, line):
+                found_balance_line = True
+                balance_line       = line
+            if not found_IBAN_line and re.findall(IBAN_line_pattern, line):
+                found_IBAN_line = True
+                IBAN_line    = line
+    
+        balance_pattern = r'\d{0,7}\.\d{0,3},\d{0,2}\sEUR'
+        current_balance_line_spltd = re.findall(balance_pattern, balance_line)[0].split()
+        self.meta_data['Balance']  = current_balance_line_spltd[0]
+        self.meta_data['Currency'] = current_balance_line_spltd[1]
+        
+        IBAN_pattern = r'[\d|\w]+'
+        current_balance_line = re.findall(IBAN_pattern, IBAN_line)
+        self.meta_data['IBAN']    = current_balance_line[1]
+        self.meta_data['BA_type'] = current_balance_line[2]
+        
+        self.current_balance   = float(self.meta_data['Balance'].replace('.','').replace(',','.'))
+        self.currency          = self.meta_data['Currency']
+        self.bank_account_type = self.meta_data['BA_type']
+        self.IBAN              = self.meta_data['IBAN']
+        print('done!')
+        
---
+++ src/helper.py
@@ -0,0 +1,218 @@
+import re
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+
+
+def is_rent(df: pd.DataFrame) -> pd.DataFrame:
+    """
+        Filter method to classify transactions with the label Rent
+    Args:
+        df: DataFrame that is queried
+
+    Returns:
+        returns all the rows of df where 'Verwendungszweck' equals 'Miet' and 'Buchungstext' is not equal to 'Gutschrift'
+        as a DataFrame
+    """
+    return (df['Verwendungszweck'].str.contains('Miet', case=False, na=False) &
+            (df['Buchungstext'].str.contains('Gutschrift', case=False, na=False) is False))
+
+
+def generate_days(first: datetime, last: datetime) -> List[datetime]:
+    """
+        Generates a list of all days from first to last inclusively
+    Args:
+        first: start date
+        last:  end date
+
+    Returns:
+        List of datetime
+    """
+    step = timedelta(days=1)
+    last += timedelta(days=1)
+    result = []
+    while first < last:
+        result.append(pd.Timestamp(first.strftime('%Y-%m-%d %H:%M:%S')))
+        first += step
+    return result
+
+
+def is_valid_csv_file(path: str) -> bool:
+    """
+    checks if path can be resolved into a csv file.
+    Args:
+        path: location that is checked
+import pandas as pd
+
+    Returns:
+        True if path can be resolved to a csv-file, False otherwise
+    """
+    my_file = Path(path)
+    if not my_file.is_file():
+        user_input = input(
+            'The path: ' + path + ' cannot be resolved to a valid file. Do you want to continue anyways? [y/n]... ')
+        if user_input not in ['y', 'Y', 'yes', 'ja', 'Ja']:
+            return False
+    else:
+        pattern = r'\.csv$'
+        if re.search(pattern, path) is None:
+            user_input = input('The passed file does not appear to be a csv file. Do you want to continue anyways? ['
+                               'y/n]... ')
+            if user_input not in ['y', 'Y', 'yes', 'ja', 'Ja']:
+                return False
+    return True
+
+
+'''
+def american_format_to_DBK(x):
+    x_split = x.split('-')
+    x_split[0], x_split[2] = x_split[2], x_split[0]
+    return '.'.join(x_split)
+
+def is_date(x):
+    if re.match(r'\d{4}-\d{2}-\d{2}', x[0]):
+        x = x.apply(lambda x: american_format_to_DBK(x))
+    x = pd.to_datetime(x, format='%d.%m.%Y')
+    return x
+    
+# Konvertiert einen String in eine Zahl
+def is_float64(x):
+    remove_dots          = lambda s: str(s).replace('.','')
+    replace_comma_by_dot = lambda s: str(s).replace(',','.')
+    float_func           = lambda s: float(s)
+    x = list(map(remove_dots,x))
+    x = list(map(replace_comma_by_dot,x))
+    x = list(map(float_func,x))
+    return x
+
+def set_date(year,month,day,hour,minute,second):
+    return datetime(year, month, day, hour, minute, second)
+
+def get_month(data, month,year, use_Werstellung = True):
+    if use_Werstellung:
+        return data[
+                        (data['Wertstellung'] >= set_date(year,month,1,0,0,0) )&
+                        (data['Wertstellung'] <= set_date(year,month+1,1,0,0,0))
+                    ]
+    else:
+        return data[
+                        (data['Buchungstag'] >= set_date(year,month,1,0,0,0) )&
+                        (data['Buchungstag'] <= set_date(year,month+1,1,0,0,0))
+                    ]
+
+def add_balance_col(data, current_balance):
+    s = [current_balance]
+    for i, transaction in enumerate(data['Betrag (EUR)']):
+        s.append(s[i]-transaction)
+    del s[-1]
+    data['Balance'] = s
+    return data
+
+def prep_table(data, current_balance): 
+    print('Converting Buchungstag to date fomat...\t\t\t', end='')
+    data['Buchungstag'] = is_date(data['Buchungstag'])
+    print('done!')
+    print('Converting Wertstellung to date fomat...\t\t', end='')
+    data['Wertstellung'] = is_date(data['Wertstellung'])
+    print('done!')
+    print('Converting Betrag (EUR) to a float column fomat...\t', end='')
+    data['Betrag (EUR)'] = is_float64(data['Betrag (EUR)'])
+    print('done!')
+    print('Sorting the table based on Wertstellung-column...\t', end='')
+    data = data.sort_values(by='Wertstellung')
+    data = data.reset_index()
+    print('done!')
+    
+    if 'Transaction Label' not in data.columns:
+        print('Adding a transaction label column...\t\t\t', end='')
+        data['Transaction Label'] = 'None'
+        print('done!')
+        
+    if 'Balance' not in data.columns:
+        print('Adding a column with the daily balance...\t\t', end='')
+        data = add_balance_col(data, current_balance)
+        print('done!')
+        
+    return data
+
+def search_for_relevant_columns(col_names):
+    if 'Buchungstag' not in col_names:
+        return False, 'Buchungstag'
+    if 'Wertstellung' not in col_names:
+        return False, 'Wertstellung' 
+    if 'Buchungstext' not in col_names:
+        return False, 'Buchungstext'
+    if 'Auftraggeber / Begünstigter' not in col_names:
+        return False, 'Auftraggeber / Begünstigter'
+    if 'Verwendungszweck' not in col_names:
+        return False, 'Verwendungszweck'
+    if 'Kontonummer' not in col_names:
+        return False, 'Kontonummer'
+    if 'BLZ' not in col_names:
+        return False, 'BLZ'
+    if 'Betrag (EUR)' not in col_names:
+        return False, 'Betrag (EUR)'
+    if 'Gläubiger-ID' not in col_names:
+        return False, 'Gläubiger-ID'
+    if 'Mandatsreferenz' not in col_names:
+        return False, 'Mandatsreferenz'
+    if 'Kundenreferenz' not in col_names:
+        return False, 'Kundenreferenz'
+    return True, ''
+
+def valid_table(data,current_balance):
+    print('Checking whether table is in expcted DKB-format...\t', end='')
+    result, name = search_for_relevant_columns(data.columns)
+    if not result:
+        raise ValueError(name + ' does not appear as a column. Please make sure that it exists and try again...')
+    else:
+        print('done!')
+        pd.set_option('display.max_columns', None)
+        data = prep_table(data, current_balance)
+        return data
+
+
+def erase_meta_data(path):
+    with open(path, "r") as f:
+        lines = f.readlines()
+
+    header_idx = -1
+    for i, line in enumerate(lines):
+        if np.min([line.find('Buchungstag'),line.find('Wertstellung'),line.find('BLZ')])> -1:
+            header_idx = i
+    
+    if header_idx > -1:
+        meta_info = lines[:header_idx]
+        lines = lines[header_idx:-1]
+    
+    with open(path + 'wo_meta.csv', "w") as f:
+        for line in lines:
+            f.write(line)
+    return path + 'wo_meta.csv'
+
+def n_months_back(n): 
+    today = datetime.now()
+    
+    m = ((today.month - (n+1)) % 12) + 1
+    if np.sign(today.month - (n+1)) > - 1:
+        y = today.year
+    else:
+        y = int(np.floor(today.year - (n+1)/12))
+    return datetime(y,m,datetime.now().day,datetime.now().hour,datetime.now().minute,datetime.now().second)
+
+def generate_days(first,last):
+    step = timedelta(days=1)
+    last += timedelta(days=1)
+    result = []
+    while first < last:
+        result.append(pd.Timestamp(first.strftime('%Y-%m-%d %H:%M:%S')))
+        #result.append(start)
+        first += step
+    return result
+    
+def is_miete(df):
+    return (df['Verwendungszweck'].str.contains('Miet',case=False,na=False) & 
+           (df['Buchungstext'].str.contains('Gutschrift',case=False,na=False) == False))
+'''
